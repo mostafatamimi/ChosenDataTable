65d0896323bb6a604fb9e4cba45b5ea8d280ab0d
mostafa <mostafatamimi@gmail.com>
2019-05-02 17:41:55 +0300

    Add project files.

---
+++ ChosenDataTable/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using ChosenDataTable.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChosenDataTable.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            List<Students> myModel = new List<Students>();
+            for(int i=0;i<10;i++)
+            {
+                myModel.Add(new Students
+                {
+                    ID=i,
+                    Name = "Name "+i,
+                    Age = i*2+3,
+                    Major = "Computer"
+                });
+            }
+            return View(myModel);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
